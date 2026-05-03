73b7ab44072eb716306f2cd6eab6096aa7481a55
Agapi Kallinikou <agapi.kallinikou@gmail.com>
2026-05-03 17:01:51 +0300

    Update main_GRAFIKA.m

--- 1_Typography_Projects/main_GRAFIKA.m
+++ 1_Typography_Projects/main_GRAFIKA.m
@@ -1,40 +1,40 @@
 % Κ α θ α ρ ´ι ζ ω
-2 clear ; clc ; close all ;
-3
-4 figure ; hold on ; axis equal ;
-5 xlim ([ -10 , 100]) ; ylim ([ -25 , 45]) ;
-6 title ( ’ Project 1 - Γ Ρ Α Φ Ι Κ Α ’) ;
-7 col = ’c ’;
-8
-9 % --- Γ ---
-10 bresenham_line (0 , 0 , 0 , 24 , col ) ;
-11 bresenham_line (11 , 24 , 0 , 24 , col ) ;
-12
-13 % --- Ρ ---
-14 bresenham_line (17 , 0 , 17 , 24 , col ) ;
-15 kappel_ellipse_right (15 , 17 , 8 , 7 , col ) ;
-16
-17 % --- Α ---
-18 bresenham_line (25 , 0 , 30 , 24 , col ) ;
-3
-19 bresenham_line (35 , 0 , 30 , 24 , col ) ;
-20 bresenham_line (27 , 11 , 33 , 11 , col ) ;
-21
-22 % --- Φ ---
-23 bresenham_line (45 , 0 , 45 , 24 , col ) ;
-24 bresenham_circle (45 , 15 , 7 , col ) ;
-25
-26 % --- Ι ---
-27 bresenham_line (58 , 0 , 58 , 24 , col ) ;
-28
-29 % --- Κ ---
-30 bresenham_line (64 , 0 , 64 , 24 , col ) ;
-31 bresenham_line (64 , 12 , 71 , 24 , col ) ;
-32 bresenham_line (64 , 12 , 71 , 0 , col ) ;
-33
-34 % --- Α ---
-35 bresenham_line (83 , 11 , 77 , 11 , col ) ;
-36 bresenham_line (85 , 0 , 80 , 24 , col ) ;
-37 bresenham_line (75 , 0 , 80 , 24 , col ) ;
-38
-39 saveas ( gcf , ’ GRAFIKA_result . png ’) ;
+clear ; clc ; close all ;
+
+figure ; hold on ; axis equal ;
+xlim ([ -10 , 100]) ; ylim ([ -25 , 45]) ;
+title ( ’ Project 1 - Γ Ρ Α Φ Ι Κ Α ’) ;
+col = ’c ’;
+
+% --- Γ ---
+bresenham_line (0 , 0 , 0 , 24 , col ) ;
+bresenham_line (11 , 24 , 0 , 24 , col ) ;
+
+% --- Ρ ---
+bresenham_line (17 , 0 , 17 , 24 , col ) ;
+kappel_ellipse_right (15 , 17 , 8 , 7 , col ) ;
+
+% --- Α ---
+bresenham_line (25 , 0 , 30 , 24 , col ) ;
+
+bresenham_line (35 , 0 , 30 , 24 , col ) ;
+bresenham_line (27 , 11 , 33 , 11 , col ) ;
+
+% --- Φ ---
+bresenham_line (45 , 0 , 45 , 24 , col ) ;
+bresenham_circle (45 , 15 , 7 , col ) ;
+
+% --- Ι ---
+bresenham_line (58 , 0 , 58 , 24 , col ) ;
+
+% --- Κ ---
+bresenham_line (64 , 0 , 64 , 24 , col ) ;
+bresenham_line (64 , 12 , 71 , 24 , col ) ;
+bresenham_line (64 , 12 , 71 , 0 , col ) ;
+
+% --- Α ---
+bresenham_line (83 , 11 , 77 , 11 , col ) ;
+bresenham_line (85 , 0 , 80 , 24 , col ) ;
+bresenham_line (75 , 0 , 80 , 24 , col ) ;
+
+saveas ( gcf , ’ GRAFIKA_result . png ’) ;
